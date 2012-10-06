3821f6fab91135586669660ee8f856b5c8864d00
Andrew Faulds <ajf@ajf.me>
2012-10-06 11:28:24 +0100

    Full achievements list page

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,21 +8,29 @@ require_once '../include/secrets.php';
 
 
 $htmlhead = <<<EOT
-    <!doctype html>
-    <meta charset=utf-8>
-    <link rel=stylesheet href=style.css>
+<!doctype html>
+<meta charset=utf-8>
+<link rel=stylesheet href=style.css>
+<div class=main>
+
+EOT;
+
+$htmlfoot = <<<EOT
+</div>
+
 EOT;
 
 $loginform = <<<EOT
-    <form method=POST action=/>
-        <h1>Welcome to sumochi</h1>
-        <p>Sumochi uses your existing GG2 forum details. Log in with them below to add an achievements list to your forum signature. (If you remove it, just log in again - you won't lose your achievements)</p>
-        <p>Once you've logged in for the first time, you will be able to use your forum details to earn achievements on approved GG2 servers.</p>
-        <input type=hidden name=p value=dologin>
-        Username: <input type=text name=username><br>
-        Password: <input type=password name=password><br>
-        <input type=submit>
-    </form>
+<form method=POST action=/>
+    <h1>Welcome to sumochi</h1>
+    <p>Sumochi uses your existing GG2 forum details. Log in with them below to add an achievements list to your forum signature. (If you remove it, just log in again - you won't lose your achievements)</p>
+    <p>Once you've logged in for the first time, you will be able to use your forum details to earn achievements on approved GG2 servers.</p>
+    <input type=hidden name=p value=dologin>
+    Username: <input type=text name=username><br>
+    Password: <input type=password name=password><br>
+    <input type=submit>
+</form>
+
 EOT;
 
 switch (isset($_REQUEST['p']) ? $_REQUEST['p'] : '') {
@@ -71,6 +79,45 @@ switch (isset($_REQUEST['p']) ? $_REQUEST['p'] : '') {
             die();
         }
     break;
+    case 'list':
+        $achievements = user_get_achievements($_GET['user']);
+        
+        if ($achievements !== NULL) {
+            echo $htmlhead;
+            echo "<h1>sumochi - " . htmlspecialchars($_GET['user']) . "</h1>\n";
+            
+            $count = count($achievements);
+            echo "<h2>Achievements ($count total)</h2>\n";
+            
+            if ($count === 0) {
+                echo "<span class=none>none</span>";
+                imageString($im, 3, 8, 36, 'none', $red);
+            } else {
+                echo "<ul class=achievements>\n";
+                foreach ($achievements as $obj) {
+                    if (user_validate_achievement($obj->key, $obj->id)) {
+                        echo "<li>\n";
+                        $prepend = '';
+                    } else {
+                        echo "<li class=invalid>\n";
+                        $prepend = '[INVALID] ';
+                    }
+                    if (property_exists($obj, 'icon')) {
+                        $base64URL = 'data:image/png;base64,' . base64_encode(file_get_contents('../media/icons/' . basename($obj->icon)));
+                        echo "<img src=\"$base64URL\" alt=\"icon\">\n";
+                    }
+                    echo $prepend . htmlspecialchars($obj->name) . "\n";
+                    echo "</li>\n";
+                }
+                echo "</ul>\n";
+            }
+            echo $htmlfoot;
+        } else {
+            echo $htmlhead;
+            echo "<h1>Error</h1>\n";
+            echo $htmlfoot;
+        }
+    break;
     case 'dologin':
         echo $htmlhead;
         $username = $_POST['username'];
@@ -84,8 +131,8 @@ switch (isset($_REQUEST['p']) ? $_REQUEST['p'] : '') {
             // generate (self-referential) signature image URL
             $img_url = where_am_i() . '?p=display&user=' . urlencode($username);
             
-            // generate (self-referential) promotional URL
-            $url = where_am_i();
+            // generate (self-referential) profile URL
+            $url = where_am_i() . '?p=list&user=' . urlencode($username);
             
             // change signature
             $sig = '[url='.$url.'][img]'.$img_url.'[/img][/url]';
@@ -100,6 +147,7 @@ switch (isset($_REQUEST['p']) ? $_REQUEST['p'] : '') {
     case '':
         echo $htmlhead;
         echo $loginform;
+        echo $htmlfoot;
     default:
         header("HTTP/1.0 404 Not Found");
     break;
